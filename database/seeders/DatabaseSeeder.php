c5b124730a341461fd686eb1e8f9c55b4093a9c2
Wayne Lai <waynelai06@gmail.com>
2024-06-10 14:22:59 +0800

    fix: user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +18,15 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' =>  Hash::make('testtest'),
+            'remember_token' => Str::random(10),
+            'dial_code' => '+60',
+            'phone' => '+60123456789',
+            'referral_code' => 'fALmcXta',
+            'role' => 'admin',
         ]);
     }
 }
